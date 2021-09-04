fa5a3175100230ac1cbb00365c85a54311215178
Alireza Karavi <karavi.alireza@gmail.com>
2021-09-04 20:29:37 +0430

    update ver

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { EnumDeviceType, EnumOperatingSystemType } from 'ntk-cms-api';
 
 export const environment = {
   production: true,
-  appVersion: '1.06.132',
+  appVersion: '1.06.133',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
   apiUrl: 'https://your-domain.com/api',
