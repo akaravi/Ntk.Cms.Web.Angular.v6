2d831a9a515d14c456e5da3471dd3098ed20379e
Alireza Karavi <karavi.alireza@gmail.com>
2022-01-04 19:08:45 +0330

    updare after merge

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { EnumDeviceType, EnumOperatingSystemType } from 'ntk-cms-api';
 
 export const environment = {
   production: true,
-  appVersion: '1.10.14.4',
+  appVersion: '1.10.14.5',
   USERDATA_KEY: 'authf649fc9a5f55',
   loadDemoMenu: false,
   loadDemoDashboard: false,
