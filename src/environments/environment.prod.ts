555ad88bec8e6ffec402467f9ce52d914f958363
Alireza Karavi <karavi.alireza@gmail.com>
2022-01-13 06:29:05 +0330

    update language

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { EnumDeviceType, EnumOperatingSystemType } from 'ntk-cms-api';
 
 export const environment = {
   production: true,
-  appVersion: '12.0.1021.1',
+  appVersion: '12.0.1022.1',
   USERDATA_KEY: 'authf649fc9a5f55',
   loadDemoMenu: false,
   loadDemoDashboard: false,
