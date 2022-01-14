940c64ece787d0441cf4bacdb9ac804bbf47be1c
Alireza Karavi <karavi.alireza@gmail.com>
2022-01-14 18:46:20 +0330

    update to angular 13

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { EnumDeviceType, EnumOperatingSystemType } from 'ntk-cms-api';
 
 export const environment = {
   production: true,
-  appVersion: '12.0.1022.1',
+  appVersion: '13.0.1023.1',
   USERDATA_KEY: 'authf649fc9a5f55',
   loadDemoMenu: false,
   loadDemoDashboard: false,
