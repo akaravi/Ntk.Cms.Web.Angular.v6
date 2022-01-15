f63d3d99035edf8bf830a52bd81a3e947cd0410f
Alireza Karavi <karavi.alireza@gmail.com>
2022-01-15 18:07:49 +0330

    laguge unpade

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ import { EnumDeviceType, EnumOperatingSystemType } from 'ntk-cms-api';
 
 export const environment = {
   production: false,
-  appVersion: '12.0.0000.0',
+  appVersion: '13.0.0000.0',
   USERDATA_KEY: 'authf649fc9a5f55',
   loadDemoMenu: true,
   loadDemoDashboard: true,
