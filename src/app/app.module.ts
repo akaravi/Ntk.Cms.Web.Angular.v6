653b311b818722959833a9bc9d3d3d154e548b96
Alireza Karavi <karavi.alireza@gmail.com>
2022-01-18 15:00:21 +0330

    remove demo theme file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-// import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { ClipboardModule } from '@angular/cdk/clipboard';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { InlineSVGModule } from 'ng-inline-svg';
@@ -11,8 +10,6 @@ import { AppRouting } from './app.routing';
 import { AppComponent } from './app.component';
 import { AuthService } from './modules/auth/_services/auth.service';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-
 import { SplashScreenModule } from './_metronic/partials/layout/splash-screen/splash-screen.module';
 import { TeardownLogic } from 'rxjs';
 import { SharedModule } from './shared/shared.module';
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -17,48 +17,9 @@ const routes: Routes = [
         loadChildren: () =>
           import('./builder/builder.module').then((m) => m.BuilderModule),
       },
-      {
-        path: 'ecommerce',
-        loadChildren: () =>
-          import('../modules/e-commerce/e-commerce.module').then(
-            (m) => m.ECommerceModule
-          ),
-      },
-      {
-        path: 'user-management',
-        loadChildren: () =>
-          import('../modules/user-management/user-management.module').then(
-            (m) => m.UserManagementModule
-          ),
-      },
-      {
-        path: 'user-profile',
-        loadChildren: () =>
-          import('../modules/user-profile/user-profile.module').then(
-            (m) => m.UserProfileModule
-          ),
-      },
-      {
-        path: 'ngbootstrap',
-        loadChildren: () =>
-          import('../modules/ngbootstrap/ngbootstrap.module').then(
-            (m) => m.NgbootstrapModule
-          ),
-      },
-      {
-        path: 'wizards',
-        loadChildren: () =>
-          import('../modules/wizards/wizards.module').then(
-            (m) => m.WizardsModule
-          ),
-      },
-      {
-        path: 'material',
-        loadChildren: () =>
-          import('../modules/material/material.module').then(
-            (m) => m.MaterialModule
-          ),
-      },
+  
+  
+
       // ** cms */
       {
         path: 'core',
