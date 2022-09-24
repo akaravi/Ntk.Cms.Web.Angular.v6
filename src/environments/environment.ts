c5ce9ce47991732fd7c724710e9f3c08a32865c7
Alireza Karavi <karavi.alireza@gmail.com>
2022-09-24 14:17:17 +0330

    update npm

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,18 +9,14 @@ export const environment = {
   leafletUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   cmsServerConfig: {
     configApiRetry: 1,
- //       configApiServerPath: 'https://apicms.ir/api/v2/',
-        // configApiServerPath: 'https://bf42-94-183-160-75.ngrok.io/api/v2/',
-  configApiServerPath: 'https://localhost:2390/api/v2/', // Test Api
-    // configApiServerPath: 'https://localhost:49155/api/v2/', // Test Api Docer
-
-
+    configApiServerPath: 'https://apicms.ir/api/v2/',
+    //configApiServerPath: 'https://bf42-94-183-160-75.ngrok.io/api/v2/',
+    // configApiServerPath: 'https://localhost:2390/api/v2/', // Test Api
+    //configApiServerPath: 'https://localhost:49155/api/v2/', // Test Api Docer
     configMvcServerPath: 'https://ntkcms.ir/',
     // configMvcServerPath: 'http://localhost:2391/', // Test Api
-
     // configHtmlBuilderServerPath: 'https://htmlbuilder.ntkcms.ir/',
     configHtmlBuilderServerPath: 'http://localhost:5000/', // Test Api
-
     configFileServerPath: 'https://apifile.ir/api/v2/',
     // configFileServerPath: 'http://localhost:2392/api/v1/' // Test Api
     configQDocServerPath: 'https://qdoc.ir/api/chat',
