2c67ed547798392400b6a294460b166a57ac866a
Alireza Karavi <karavi.alireza@gmail.com>
2022-09-28 11:34:20 +0330

    update  angular core

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ntk-cms-web-angular-v2'),
+      dir: require('path').join(__dirname, './coverage/ntk-cms-web-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
