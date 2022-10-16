d8e3a2443b4658559ff8fabc57760f45fad16f24
mohamad mahvash <mohammadmahvash1@gmail.com>
2022-10-16 14:41:09 +0330

    update version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { EnumDeviceType, EnumOperatingSystemType } from 'ntk-cms-api';
 export const environment = {
   production: true,
   checkAccess:false,
-  appVersion: '14.1.0721.1',
+  appVersion: '14.1.0724.1',
   authKey: 'authf649fc9a5f55',
   loadDemoDashboard: false,
   ProgressConsoleLog: false,
