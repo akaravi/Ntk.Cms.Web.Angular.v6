e3de852575fe641ed9a7c95b4afc2e85ae182536
mohamad mahvash <mohammadmahvash1@gmail.com>
2022-11-20 17:00:45 +0330

    create customer-order-result

---
+++ src/app/cms-modules/estate/customer-order-result/list/list.component.ts
@@ -0,0 +1,361 @@
+
+import { ActivatedRoute, Router } from '@angular/router';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import {
+  EnumSortType,
+  ErrorExceptionResult,
+  FilterModel,
+  TokenInfoModel,
+  FilterDataModel,
+  EnumRecordStatus,
+  DataFieldInfoModel,
+  EstateCustomerOrderResultModel,
+  EstateCustomerOrderResultService,
+} from 'ntk-cms-api';
+import { ComponentOptionSearchModel } from 'src/app/core/cmsComponentModels/base/componentOptionSearchModel';
+import { PublicHelper } from 'src/app/core/helpers/publicHelper';
+import { ProgressSpinnerModel } from 'src/app/core/models/progressSpinnerModel';
+import { CmsToastrService } from 'src/app/core/services/cmsToastr.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ComponentOptionExportModel } from 'src/app/core/cmsComponentModels/base/componentOptionExportModel';
+import { ComponentOptionStatistModel } from 'src/app/core/cmsComponentModels/base/componentOptionStatistModel';
+import { MatSort } from '@angular/material/sort';
+import { PageEvent } from '@angular/material/paginator';
+import { Subscription } from 'rxjs';
+import { CmsConfirmationDialogService } from 'src/app/shared/cms-confirmation-dialog/cmsConfirmationDialog.service';
+import { TokenHelper } from 'src/app/core/helpers/tokenHelper';
+import { TranslateService } from '@ngx-translate/core';
+import { EstateCustomerOrderResultComponent } from '../view/view.component';
+@Component({
+  selector: 'app-estate-customer-order-result-list',
+  templateUrl: './list.component.html',
+})
+export class EstateCustomerOrderResultListComponent implements OnInit, OnDestroy {
+  requestLinkCustomerOrder = '';
+  requestLinkProperty = '';
+  constructor(
+    private estateCustomerOrderResultService: EstateCustomerOrderResultService,
+    public publicHelper: PublicHelper,
+    private activatedRoute: ActivatedRoute,
+    private cmsToastrService: CmsToastrService,
+    private cmsConfirmationDialogService: CmsConfirmationDialogService,
+    private router: Router,
+    private tokenHelper: TokenHelper,
+    private cdr: ChangeDetectorRef,
+    public translate: TranslateService,
+    public dialog: MatDialog) {
+    this.loading.cdr = this.cdr;
+    this.loading.message = this.translate.instant('MESSAGE.Receiving_information');
+    this.requestLinkCustomerOrder = this.activatedRoute.snapshot.paramMap.get('LinkCustomerOrder');
+    this.requestLinkProperty = this.activatedRoute.snapshot.paramMap.get('LinkProperty');
+    this.optionsSearch.parentMethods = {
+      onSubmit: (model) => this.onSubmitOptionsSearch(model),
+    };
+    this.optionsExport.parentMethods = {
+      onSubmit: (model) => this.onSubmitOptionExport(model),
+    };
+    /*filter Sort*/
+    this.filteModelContent.sortColumn = 'CreatedDate';
+    this.filteModelContent.sortType = EnumSortType.Descending;
+
+    if (
+      this.requestLinkCustomerOrder &&
+      this.requestLinkCustomerOrder.length > 0
+    ) {
+      const filter = new FilterDataModel();
+      if (this.requestLinkCustomerOrder && this.requestLinkCustomerOrder.length > 0) {
+        filter.propertyName = 'LinkCustomerOrder';
+        filter.value = this.requestLinkCustomerOrder;
+        this.filteModelContent.filters.push(filter);
+      }
+    }
+    if (
+      this.requestLinkProperty &&
+      this.requestLinkProperty.length > 0
+    ) {
+      const filter = new FilterDataModel();
+      if (this.requestLinkProperty && this.requestLinkProperty.length > 0) {
+        filter.propertyName = 'LinkProperty';
+        filter.value = this.requestLinkProperty;
+        this.filteModelContent.filters.push(filter);
+      }
+    }
+  }
+  comment: string;
+  author: string;
+  dataSource: any;
+  flag = false;
+  tableContentSelected = [];
+
+  filteModelContent = new FilterModel();
+  dataModelResult: ErrorExceptionResult<EstateCustomerOrderResultModel> = new ErrorExceptionResult<EstateCustomerOrderResultModel>();
+
+  optionsSearch: ComponentOptionSearchModel = new ComponentOptionSearchModel();
+  optionsStatist: ComponentOptionStatistModel = new ComponentOptionStatistModel();
+  optionsExport: ComponentOptionExportModel = new ComponentOptionExportModel();
+  tokenInfo = new TokenInfoModel();
+  loading = new ProgressSpinnerModel();
+  tableRowsSelected: Array<EstateCustomerOrderResultModel> = [];
+  tableRowSelected: EstateCustomerOrderResultModel = new EstateCustomerOrderResultModel();
+  tableSource: MatTableDataSource<EstateCustomerOrderResultModel> = new MatTableDataSource<EstateCustomerOrderResultModel>();
+  fieldsInfo: Map<string, DataFieldInfoModel> = new Map<string, DataFieldInfoModel>();
+
+
+  tabledisplayedColumns: string[] = [];
+  tabledisplayedColumnsSource: string[] = [
+    'Description',
+  ];
+
+  expandedElement: EstateCustomerOrderResultModel | null;
+  cmsApiStoreSubscribe: Subscription;
+
+  ngOnInit(): void {
+
+    this.tokenHelper.getCurrentToken().then((value) => {
+      this.tokenInfo = value;
+      this.DataGetAll();
+    });
+    this.cmsApiStoreSubscribe = this.tokenHelper.getCurrentTokenOnChange().subscribe((next) => {
+      this.tokenInfo = next;
+      this.DataGetAll();
+    });
+
+  }
+
+  ngOnDestroy(): void {
+    this.cmsApiStoreSubscribe.unsubscribe();
+  }
+  DataGetAll(): void {
+    this.tabledisplayedColumns = this.publicHelper.TabledisplayedColumnsCheckByAllDataAccess(this.tabledisplayedColumnsSource, [], this.tokenInfo);
+
+    this.tableRowsSelected = [];
+    this.tableRowSelected = new EstateCustomerOrderResultModel();
+    const pName = this.constructor.name + 'main';
+    this.loading.Start(pName, this.translate.instant('MESSAGE.get_information_list'));
+    this.filteModelContent.accessLoad = true;
+    /*filter CLone*/
+    const filterModel = JSON.parse(JSON.stringify(this.filteModelContent));
+    /*filter CLone*/
+
+    this.estateCustomerOrderResultService.ServiceGetAll(filterModel).subscribe({
+      next: (ret) => {
+        this.fieldsInfo = this.publicHelper.fieldInfoConvertor(ret.access);
+
+        if (ret.isSuccess) {
+          this.dataModelResult = ret;
+          this.tableSource.data = ret.listItems;
+
+          if (this.optionsSearch.childMethods) {
+            this.optionsSearch.childMethods.setAccess(ret.access);
+          }
+        }
+        else {
+          this.cmsToastrService.typeErrorMessage(ret.errorMessage);
+        }
+        this.loading.Stop(pName);
+      },
+      error: (er) => {
+        this.cmsToastrService.typeError(er);
+        this.loading.Stop(pName);
+      }
+    }
+    );
+  }
+
+
+  onTableSortData(sort: MatSort): void {
+    if (this.tableSource && this.tableSource.sort && this.tableSource.sort.active === sort.active) {
+      if (this.tableSource.sort.start === 'asc') {
+        sort.start = 'desc';
+        this.filteModelContent.sortColumn = sort.active;
+        this.filteModelContent.sortType = EnumSortType.Descending;
+      } else if (this.tableSource.sort.start === 'desc') {
+        this.filteModelContent.sortColumn = '';
+        this.filteModelContent.sortType = EnumSortType.Ascending;
+      } else {
+        sort.start = 'desc';
+      }
+    } else {
+      this.filteModelContent.sortColumn = sort.active;
+      this.filteModelContent.sortType = EnumSortType.Ascending;
+    }
+    this.tableSource.sort = sort;
+    this.filteModelContent.currentPageNumber = 0;
+  }
+  onTablePageingData(event?: PageEvent): void {
+    this.filteModelContent.currentPageNumber = event.pageIndex + 1;
+    this.filteModelContent.rowPerPage = event.pageSize;
+  }
+
+
+
+
+  onActionbuttonDeleteRow(model: EstateCustomerOrderResultModel = this.tableRowSelected): void {
+    if (!model || !model.id || model.id.length === 0) {
+      const emessage = this.translate.instant('MESSAGE.no_row_selected_to_delete');
+      this.cmsToastrService.typeErrorSelected(emessage);
+      return;
+    }
+    this.tableRowSelected = model;
+
+    if (
+      this.dataModelResult == null ||
+      this.dataModelResult.access == null ||
+      !this.dataModelResult.access.accessDeleteRow
+    ) {
+      this.cmsToastrService.typeErrorAccessDelete();
+      return;
+    }
+
+
+    const title = this.translate.instant('MESSAGE.Please_Confirm');
+    const message = this.translate.instant('MESSAGE.Do_you_want_to_delete_this_content') + '?' + '<br> ( ' + this.tableRowSelected.id + ' ) ';
+    this.cmsConfirmationDialogService.confirm(title, message)
+      .then((confirmed) => {
+        if (confirmed) {
+          const pName = this.constructor.name + 'main';
+          this.loading.Start(pName);
+
+          this.estateCustomerOrderResultService.ServiceDelete(this.tableRowSelected.id).subscribe({
+            next: (ret) => {
+              if (ret.isSuccess) {
+                this.cmsToastrService.typeSuccessRemove();
+                this.DataGetAll();
+              } else {
+                this.cmsToastrService.typeErrorRemove();
+              }
+              this.loading.Stop(pName);
+            },
+            error: (er) => {
+              this.cmsToastrService.typeError(er);
+              this.loading.Stop(pName);
+            }
+          }
+          );
+        }
+      }
+      )
+      .catch(() => {
+        // console.log('User dismissed the dialog (e.g., by using ESC, clicking the cross icon, or clicking outside the dialog)')
+      }
+      );
+
+  }
+
+  onActionbuttonStatist(): void {
+    this.optionsStatist.data.show = !this.optionsStatist.data.show;
+    if (!this.optionsStatist.data.show) {
+      return;
+    }
+    const statist = new Map<string, number>();
+    statist.set('Active', 0);
+    statist.set('All', 0);
+    this.estateCustomerOrderResultService.ServiceGetCount(this.filteModelContent).subscribe({
+      next: (ret) => {
+        if (ret.isSuccess) {
+          statist.set('All', ret.totalRowCount);
+          this.optionsStatist.childMethods.setStatistValue(statist);
+        } else {
+          this.cmsToastrService.typeErrorMessage(ret.errorMessage);
+        }
+      },
+      error: (er) => {
+        this.cmsToastrService.typeError(er);
+      }
+    }
+    );
+
+    const filterStatist1 = JSON.parse(JSON.stringify(this.filteModelContent));
+    const fastfilter = new FilterDataModel();
+    fastfilter.propertyName = 'RecordStatus';
+    fastfilter.value = EnumRecordStatus.Available;
+    filterStatist1.filters.push(fastfilter);
+    this.estateCustomerOrderResultService.ServiceGetCount(filterStatist1).subscribe({
+      next: (ret) => {
+        if (ret.isSuccess) {
+          statist.set('Active', ret.totalRowCount);
+          this.optionsStatist.childMethods.setStatistValue(statist);
+        } else {
+          this.cmsToastrService.typeErrorMessage(ret.errorMessage);
+        }
+      },
+      error: (er) => {
+        this.cmsToastrService.typeError(er);
+      }
+    }
+    );
+
+  }
+
+  onActionbuttonExport(): void {
+    this.optionsExport.data.show = !this.optionsExport.data.show;
+    this.optionsExport.childMethods.setExportFilterModel(this.filteModelContent);
+  }
+  onSubmitOptionExport(model: FilterModel): void {
+    const exportlist = new Map<string, string>();
+    exportlist.set('Download', 'loading ... ');
+    this.estateCustomerOrderResultService.ServiceExportFile(model).subscribe({
+      next: (ret) => {
+        if (ret.isSuccess) {
+          exportlist.set('Download', ret.linkFile);
+          this.optionsExport.childMethods.setExportLinkFile(exportlist);
+        } else {
+          this.cmsToastrService.typeErrorMessage(ret.errorMessage);
+        }
+      },
+      error: (er) => {
+        this.cmsToastrService.typeError(er);
+      }
+    }
+    );
+  }
+
+  onActionbuttonViewRow(model: EstateCustomerOrderResultModel = this.tableRowSelected): void {
+    if (!model || !model.id || model.id.length === 0) {
+      this.cmsToastrService.typeErrorSelectedRow();
+      return;
+    }
+    this.tableRowSelected = model;
+    if (
+      this.dataModelResult == null ||
+      this.dataModelResult.access == null ||
+      !this.dataModelResult.access.accessWatchRow
+    ) {
+      this.cmsToastrService.typeErrorAccessWatch();
+      return;
+    }
+    const dialogRef = this.dialog.open(EstateCustomerOrderResultComponent, {
+      height: '90%',
+      data: { id: this.tableRowSelected.id }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result && result.dialogChangedDate) {
+      }
+    });
+  }
+  onActionbuttonReload(): void {
+    this.DataGetAll();
+  }
+  onSubmitOptionsSearch(model: any): void {
+    this.filteModelContent.filters = model;
+    this.DataGetAll();
+  }
+  onActionTableRowSelect(row: EstateCustomerOrderResultModel): void {
+    this.tableRowSelected = row;
+
+    if (!row["expanded"])
+      row["expanded"] = false;
+    row["expanded"] = !row["expanded"]
+  }
+  onActionTableRowMouseEnter(row: EstateCustomerOrderResultModel): void {
+    this.tableRowSelected = row;
+    row["expanded"] = true;
+  }
+  onActionTableRowMouseLeave(row: EstateCustomerOrderResultModel): void {
+    row["expanded"] = false;
+  }
+  onActionBackToParent(): void {
+    this.router.navigate(['/estate/customer-order/']);
+  }
+}
