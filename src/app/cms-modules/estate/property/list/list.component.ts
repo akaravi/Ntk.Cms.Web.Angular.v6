732b0736d67d137ccd6d9bf98bf0fbacbe42c2d1
mohamad mahvash <mohammadmahvash1@gmail.com>
2022-12-11 14:05:37 +0330

    indo

--- src/app/cms-modules/estate/property/list/list.component.ts
+++ src/app/cms-modules/estate/property/list/list.component.ts
@@ -579,6 +579,8 @@ export class EstatePropertyListComponent
     const statist = new Map<string, number>();
     statist.set("Active", 0);
     statist.set("All", 0);
+    const pName = this.constructor.name + '.ServiceExportFile';
+    this.loading.Start(pName, this.translate.instant('MESSAGE.Get_the_output_file'));
     this.contentService
       .ServiceGetCount(this.filteModelProperty)
       .subscribe({
@@ -589,9 +591,11 @@ export class EstatePropertyListComponent
           } else {
             this.cmsToastrService.typeErrorMessage(ret.errorMessage);
           }
+          this.loading.Stop(pName);
         },
         error: (er) => {
           this.cmsToastrService.typeError(er);
+          this.loading.Stop(pName);
         }
       }
       );
@@ -609,9 +613,11 @@ export class EstatePropertyListComponent
         } else {
           this.cmsToastrService.typeErrorMessage(ret.errorMessage);
         }
+        this.loading.Stop(pName);
       },
       error: (er) => {
         this.cmsToastrService.typeError(er);
+        this.loading.Stop(pName);
       }
     }
     );
