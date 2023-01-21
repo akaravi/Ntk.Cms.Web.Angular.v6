dcb96dd548fdee758b1cec472b09923918eaff19
pipeline <pipeline@ntk.ir>
2023-01-21 14:41:48 +0330

    OPTIMAZE

--- src/app/cms-modules/estate/customer-order/add/add.component.ts
+++ src/app/cms-modules/estate/customer-order/add/add.component.ts
@@ -101,6 +101,7 @@ export class EstateCustomerOrderAddComponent implements OnInit {
   // ** Accardon */
   step = 0;
   hidden = true;
+  resultSortArrowIcon = true;
   areaAddressView = false;
   ngOnInit(): void {
     this.formInfo.formTitle = this.translate.instant('TITLE.ADD');
